5d8df0959430f772dc961525cb2db4707353c057
admin123 <lobxy>
2021-10-09 15:41:38 +0530

    First Commit

---
+++ app/src/main/java/com/lovish/mvvmapp/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lovish.mvvmapp.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lovish.mvvmapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/lovish/mvvmapp/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.lovish.mvvmapp.splash;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lovish.mvvmapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
